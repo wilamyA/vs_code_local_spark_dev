cac48d075a4c4248a6a2834959b3713893bad4ab
wilamyA <wilamy_ribeiro_amaral@hotmail.com>
2024-01-28 20:00:25 +0000

    add module with global variables

---
+++ app_name/configs/global_variables.py
@@ -0,0 +1,6 @@
+MINIO_ENDPOINT = "http://storage:9000"
+AWS_ACCESS_KEY_ID = "root"
+AWS_SECRET_ACCESS_KEY = "password"
+
+BUCKET_ORIGIN = "sor"
+PREFIX_BUCKET_ORIGIN = "football/data/"
---
+++ app_name/jobs/lineups.py
@@ -0,0 +1,2 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
